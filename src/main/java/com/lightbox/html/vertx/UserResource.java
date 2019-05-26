d320b97368b69a93589d756aa73374d9f573bf19
strogiyotec <almas337519@gmail.com>
2019-05-26 21:32:30 +0600

    add cors options example

--- src/main/java/com/lightbox/html/vertx/UserResource.java
+++ src/main/java/com/lightbox/html/vertx/UserResource.java
@@ -19,26 +19,84 @@ public final class UserResource extends AbstractVerticle {
      */
     private static final int PORT = 9090;
 
+    /**
+     * List of users.
+     */
+    private final JsonArray users =
+            new JsonArray()
+                    .add(
+                            new JsonObject().put("name", "Almas").put("age", 22)
+                    );
+
     @Override
     public void start() {
         final Router router = Router.router(this.vertx);
+        this.users(router);
+        this.corsOptions(router);
+        this.saveUser(router);
+
+        this.vertx.createHttpServer()
+                .requestHandler(router::accept)
+                .listen(PORT, event -> {
+                    if (event.failed()) {
+                        throw new RuntimeException(event.cause());
+                    } else {
+                        System.out.println("Rest client in port 9090 is started");
+                    }
+                });
+    }
+
+    /**
+     * Init get users endpoint.
+     *
+     * @param router Vertx Router
+     */
+    private void users(final Router router) {
         router.route(HttpMethod.GET, "/users")
                 .handler(request -> {
                     final HttpServerResponse response = request.response();
                     response.putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/json")
+                            .putHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN.toString(), request.request().getHeader("Origin"))
                             .setStatusCode(HttpResponseStatus.OK.code())
                             .end(
-                                    new JsonObject().put(
-                                            "users",
-                                            new JsonArray().add(new JsonObject().put("name", "Almas").put("age", 22))
-                                    ).toString()
+                                    new JsonObject()
+                                            .put("users", this.users)
+                                            .toString()
                             );
                 });
-        this.vertx.createHttpServer().requestHandler(router::accept).listen(PORT, event -> {
-            if (event.failed()) {
-                throw new RuntimeException(event.cause());
-            }
-        });
-        System.out.println("Rest client in port 9090 is started");
+    }
+
+    /**
+     * Endpoint to save new user
+     *
+     * @param router Vertx router
+     */
+    private void saveUser(final Router router) {
+        router.route(HttpMethod.POST, "/users")
+                .handler(request -> {
+                    this.users.add(request.getBodyAsJson());
+                    final HttpServerResponse response = request.response();
+                    response.putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/json")
+                            .setStatusCode(HttpResponseStatus.OK.code())
+                            .end(
+                                    new JsonObject()
+                                            .put("users", this.users)
+                                            .toString()
+                            );
+                });
+    }
+
+    /**
+     * Init options request for CORS.
+     *
+     * @param router Vertx Router
+     */
+    private void corsOptions(final Router router) {
+        router.route(HttpMethod.OPTIONS, "/users")
+                .handler(request -> {
+                    final HttpServerResponse response = request.response();
+                    response.setStatusCode(HttpResponseStatus.OK.code())
+                            .end();
+                });
     }
 }
